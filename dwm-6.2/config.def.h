65c6add8ce93e27a68b5312ad377c97588fc02c2
Sleroq <52239427+sleroq@users.noreply.github.com>
2021-12-26 21:47:59 +0300

    Updated keybindings

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -64,13 +64,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *fmanager[]  = { "nautilus", NULL };
 static const char *flameshot[]  = { "flameshot", "gui" };
+static const char *randombackground[]  = { "feh", "--bg-fill", "-z", "/home/sloq/Pictures/wallpapers" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ NULL,                         XK_Print,  spawn,          {.v = flameshot } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = fmanager } },
+	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = randombackground } },
 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -83,7 +87,7 @@ static Key keys[] = {
 
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
