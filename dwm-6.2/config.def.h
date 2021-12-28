0e635f97f0a7da01c5e2f699417db425f380f64c
Sleroq <52239427+sleroq@users.noreply.github.com>
2021-12-29 00:27:43 +0300

    something

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -65,8 +65,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *fmanager[]  = { "nautilus", NULL };
-static const char *flameshot[]  = { "flameshot", "gui" };
-static const char *randombackground[]  = { "feh", "--bg-fill", "-z", "/home/sloq/Pictures/wallpapers" };
+static const char *flameshot[]  = { "flameshot", "gui", NULL };
+static const char *randombackground[]  = { "feh", "--bg-fill", "-z", "/home/sloq/Pictures/wallpapers", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -94,7 +94,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ ControlMask|MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
