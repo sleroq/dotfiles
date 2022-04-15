e5145885f4589a921e06902039fc80d299598df0
Sleroq <cantundo@pm.me>
2022-04-15 20:24:49 +0300

    Added "Alt-tab"

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -250,6 +250,20 @@ globalkeys = gears.table.join(
         end,
         {description = "focus previous by index", group = "client"}
     ),
+
+    awful.key({ "Mod1",           }, "Tab",
+        function ()
+            awful.client.focus.byidx( 1)
+        end,
+        {description = "focus next by index", group = "client"}
+    ),
+    awful.key({ "Mod1", "Shift"   }, "Tab",
+        function ()
+            awful.client.focus.byidx(-1)
+        end,
+        {description = "focus previous by index", group = "client"}
+    ),
+
     awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
               {description = "show main menu", group = "awesome"}),
 
