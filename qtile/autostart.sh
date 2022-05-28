f758c47be4571daa3c0ba19ee54bc54713f02929
Sleroq <cantundo@pm.me>
2022-05-28 22:30:02 +0300

    Meow!

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -13,14 +13,17 @@ run sleep 2
 # For sudo apps (disk mounting)
 run /usr/lib/polkit-kde-authentication-agent-1
 
-run setxkbmap -layout us,ru -option grp:win_space_toggle
-
+# Conpositor
 run picom
 
+# Screenshots
 run flameshot
 
+# Network control
 run nm-applet
 
+# Sound control
 run pasystray -g
 
+# Wallpapers
 feh --bg-max -z -r ~/Sync/валпаперс
