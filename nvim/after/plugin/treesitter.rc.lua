b5b46c0135d97bf45a1356e6ddf42acffc807c8d
Sleroq <52239427+sleroq@users.noreply.github.com>
2022-09-10 18:15:56 +0300

    Added and configured tree-sitter plugin (smart colors)

---
+++ nvim/after/plugin/treesitter.rc.lua
@@ -0,0 +1,41 @@
+local status, ts = pcall(require, 'nvim-treesitter.configs')
+if (not status) then return end
+
+ts.setup {
+  ensure_installed = {
+    'toml',
+    'json',
+    'html',
+    'lua',
+    'bash',
+    'css',
+    'go',
+    'markdown'
+  },
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+
+  indent = {
+    enable = true,
+    disable = {},
+  },
+}
+
+-- vim.opt.foldmethod     = 'expr'
+-- vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
+---WORKAROUND - it's all folded >_<
+-- vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
+--   group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+--   callback = function()
+--     vim.opt.foldmethod     = 'expr'
+--     vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
+--   end
+-- })
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -19,5 +19,9 @@ packer.startup(function(use)
   use 'hrsh7th/nvim-cmp' -- Completion
   use 'neovim/nvim-lspconfig' -- LSP
   use 'L3MON4D3/LuaSnip'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  }
 end)
 
