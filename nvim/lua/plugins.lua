143929e92c23d3327095dcd109d79e829ff39197
Sleroq <52239427+sleroq@users.noreply.github.com>
2022-09-10 20:18:09 +0300

    Added autotag and autopairs plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,7 +4,7 @@ if (not status) then
   return
 end
 
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
@@ -14,11 +14,16 @@ packer.startup(function(use)
   }
   use 'nvim-lualine/lualine.nvim' -- Statusline
   use 'onsails/lspkind-nvim' -- vscode-like pictograms
-  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
-  use 'hrsh7th/nvim-cmp' -- Completion
   use 'neovim/nvim-lspconfig' -- LSP
   use 'L3MON4D3/LuaSnip'
+
+  use 'hrsh7th/nvim-cmp' -- Completion
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+
   use {
     'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
