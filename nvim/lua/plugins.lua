171ca9cc6d60321de7670a1080f17b3cec1a7149
Sleroq <52239427+sleroq@users.noreply.github.com>
2022-09-10 18:14:07 +0300

    Added and configured lsp and completion plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -13,5 +13,11 @@ packer.startup(function(use)
     requires = { 'tjdevries/colorbuddy.nvim' }
   }
   use 'nvim-lualine/lualine.nvim' -- Statusline
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp' -- Completion
+  use 'neovim/nvim-lspconfig' -- LSP
+  use 'L3MON4D3/LuaSnip'
 end)
 
