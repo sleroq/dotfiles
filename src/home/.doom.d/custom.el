3a3b20aba7aea8e287daffd47bcc0f872328c835
Sleroq <cantundo@pm.me>
2022-11-16 22:46:10 +0300

    Added custom.el

---
+++ src/home/.doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-safe-remote-resources
+   '("\\`https://gitlab\\.com/OlMon/org-themes/-/raw/master/src/white_clean/white_clean\\.theme\\'" "\\`https://gitlab\\.com/OlMon/org-themes/-/blob/master/src/white_clean/white_clean\\.theme\\'" "\\`https://raw\\.githubusercontent\\.com/alphapapa/org-html-theme-darksun/master/export/setup/theme-darksun\\.setup\\'")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
