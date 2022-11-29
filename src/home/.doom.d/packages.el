4d8c462e011a0a9c96cc5a53974f96d04ba9a72e
Sleroq <cantundo@pm.me>
2022-11-30 00:27:29 +0300

    yuck-mode

--- src/home/.doom.d/packages.el
+++ src/home/.doom.d/packages.el
@@ -17,6 +17,8 @@
 (package! ox-gemini)
 (package! elpher)
 
+(package! yuck-mode)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
