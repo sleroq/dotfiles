948f07d9817560e308e8f4bd4564f765034713f7
Sleroq <cantundo@pm.me>
2022-12-31 14:53:17 +0300

    Moved environment variables in zsh config

--- src/home/.oh-my-zsh/custom/everything.zsh
+++ src/home/.oh-my-zsh/custom/everything.zsh
@@ -1,12 +1,6 @@
 # Aliases for sudo
 alias sudo='sudo '
 
-# local path
-path+=("$HOME/.local/bin")
-
-# Scripts
-path+=("$HOME/develop/other/dotfiles/scripts")
-
 # XBPS package manager
 alias update='xbps-install -Su'
 # alias xi=xbps-install
@@ -27,7 +21,6 @@ if [ -d $HOME/.nix-profile/etc/profile.d ]; then
 fi
 
 # Doom Emacs
-path+=("$HOME/.emacs.d/bin")
 alias e='emacsclient -c --tty --alternate-editor="nvim" %F'
 
 # NeoVim
@@ -40,32 +33,3 @@ alias cd=z
 
 # Tmux
 alias tmux='tmux -f ~/.config/tmux/tmux.conf'
-
-# Node.js
-path+=("$HOME/develop/node.js/bin")
-export N_PREFIX=~/develop/node.js
-
-# Golang
-export GOPATH=$HOME/develop/go
-path+=("$(go env GOPATH)/bin")
-
-# Wayland
-if [[ -z DESKTOP_SESSION ]]
-then
-  export MOZ_ENABLE_WAYLAND=1
-  export QT_QPA_PLATFORM=wayland-egl
-  export ELM_DISPLAY=wl
-  export SDL_VIDEODRIVER=wayland
-  export _JAVA_AWT_WM_NONREPARENTING=1
-  export XDG_CURRENT_DESKTOP=sway
-  export XDG_SESSION_DESKTOP=sway
-fi
-
-# Services
-export SVDIR=~/.config/service
-
-# Safe place
-export SAFE_PLACE=/tmp/vault
-
-# Timezone
-export TZ=Europe/Moscow
