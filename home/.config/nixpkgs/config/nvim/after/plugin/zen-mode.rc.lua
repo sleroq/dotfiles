eb698c170e33a9cb1ecaedae37057b0c97b98273
Sleroq <sleroq@sleroq.link>
2023-02-03 07:32:04 +0300

    Moved neovim config and switched to LazyVim

--- home/.config/nixpkgs/config/nvim/after/plugin/zen-mode.rc.lua
+++
@@ -1,6 +0,0 @@
-local status, zenmode = pcall(require, 'zen-mode')
-if (not status) then return end
-
-zenmode.setup()
-
-vim.keymap.set('n', '<C-w>o', '<cmd>ZenMode<cr>', { silent = true })
--- home/.config/nixpkgs/config/nvim/scripts/format.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
-
-cd "$SCRIPT_PATH"
-cd ..
-
-find . -iname '*.lua' -exec lua-format {} -i \;
