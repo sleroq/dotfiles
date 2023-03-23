c605dcfcf8ed328141e3cf63faa0316586d8f27c
Sleroq <hireme@sleroq.link>
2023-03-23 23:29:00 +0300

    Enabled linking picom config

--- home/modules/wms/default.nix
+++ home/modules/wms/default.nix
@@ -1,12 +1,21 @@
-{ config, pkgs, ... }:
+{ config, pkgs, opts, lib, ... }:
 
+with lib;
 with config;
 {
   imports = [
     ./leftwm.nix
     ./sway.nix
   ];
+
+  home.activation.picom = hm.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD ln -sfn $VERBOSE_ARG \
+        ${opts.realConfigs}/picom.conf $HOME/.config
+  '';
+
   home.packages = with pkgs; [
     networkmanagerapplet
+    ulauncher
+    picom
   ];
 }
