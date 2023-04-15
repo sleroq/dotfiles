12dbcb27d28f08803fb1554eef6bd73d0d4da61c
Sleroq <hireme@sleroq.link>
2023-04-15 07:49:47 +0300

    Minor

--- home/config/emacs/custom.el
+++ home/config/emacs/custom.el
@@ -6,7 +6,8 @@
  '(custom-safe-themes
    '("10fef6d73ae453f39c9f325915386d41894870b72926e8e9b0c39d030447b703" default))
  '(org-safe-remote-resources
-   '("\\`https://gitlab\\.com/OlMon/org-themes/-/raw/master/src/white_clean/white_clean\\.theme\\'")))
+   '("\\`https://gitlab\\.com/OlMon/org-themes/-/raw/master/src/white_clean/white_clean\\.theme\\'"))
+ '(warning-suppress-types '((initialization))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
