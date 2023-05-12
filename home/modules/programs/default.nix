18e583f5a4f8d059faae68f2c79489275a56cc10
Sleroq <hireme@sleroq.link>
2023-05-12 19:18:42 +0300

    Added neofetch configuration, updated app list

--- home/modules/programs/default.nix
+++ home/modules/programs/default.nix
@@ -1,73 +1,58 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
-with config;
 {
   imports = [
     ./tmux.nix
     ./lf.nix
     ./zathura.nix
     ./kitty.nix
-    ./foot.nix
+    ./neofetch.nix
   ];
 
   services.flameshot.enable = true;
-  services.safeeyes.enable = true;
-  services.kdeconnect.enable = true;
-  systemd.user.services.ss = {
-    Install.WantedBy = [ "multi-user.target" ];
-    # Install.After = [ "network.target" ];
-    Unit.Description = "Shadowsocks reverse proxy whatever meow meow";
-    Service = {
-      ExecStart = ''${pkgs.bore-cli}/bin/bore local 8666 --to 5.252.25.99 --port 40004'';         
+  programs.mpv = {
+    enable = true;
+    config = {
+      volume = 50;
     };
   };
 
   home.packages = with pkgs; [
-    sonixd
-    # filezilla
+    # feishin waiting for merge request to be accepted
+    picard
+    rclone
     tdesktop
     discord
     schildichat-desktop
     chromium
     exodus
     krita
-    ngrok
     gimp
-    # libreoffice-fresh
+    libreoffice-fresh
     libsForQt5.kdenlive
-    mpv
     obs-studio
     networkmanager-openvpn
     qbittorrent
     keepassxc
-    logseq
     syncplay
+    rustdesk
+
+    ffmpegthumbnailer
+    cinnamon.nemo-with-extensions
 
     # CLI
     gdb
-    thefuck
     gocryptfs
-    stow
     xclip
-    onefetch
-    btop
     bore-cli
     ngrok
-    neofetch
     ffmpeg
-    # shadowsocks-libev
-    # shadowsocks-v2ray-plugin
 
     # Work
     skypeforlinux
     remmina
     nomachine-client
-    # rustdesk
 
-    # Hacking
-    # pyrit2
-    # wifite2
-    # macchanger
     iw
   ];
 }
