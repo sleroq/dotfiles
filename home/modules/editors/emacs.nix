b0fe3b18900d0abc49c39d3a84101febb473604d
Sleroq <hireme@sleroq.link>
2023-06-17 18:22:56 +0300

    Fixed doom emacs installation

--- home/modules/editors/emacs.nix
+++ home/modules/editors/emacs.nix
@@ -1,83 +1,85 @@
-{ config, pkgs, lib, opts, inputs, ... }:
+{ config, pkgs, opts, lib, inputs, ... }:
 
 with lib;
+let repoUrl = "https://github.com/doomemacs/doomemacs";
+in
 {
-  nixpkgs.overlays = [ inputs.emacs-overlay.overlay ];
-  services.emacs = {
-    enable = true;
-    package = pkgs.emacs;
-  };
+  config = {
+    nixpkgs.overlays = [ inputs.emacs-overlay.overlay ];
 
-  # home.activation.emacs = hm.dag.entryAfter [ "writeBoundary" ] ''
-  #   $DRY_RUN_CMD ln -sfn $VERBOSE_ARG \
-  #       ${opts.realConfigs}/.doom.d $HOME
-  # '';
+    fonts.fontconfig.enable = true;
+    home.packages = with pkgs; [
+      ## Emacs itself
+      binutils # native-comp needs 'as', provided by this
+      # 28.2 + native-comp
+      # ((emacsPackagesFor emacs-unstable).emacsWithPackages
+      ((emacsPackagesFor emacs).emacsWithPackages
+        (epkgs: [ epkgs.vterm ]))
 
-  home.activation.emacs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    $DRY_RUN_CMD ln -sfn $VERBOSE_ARG \
-        ${opts.realConfigs}/emacs $HOME/.config
-  '';
+      ## Doom dependencies
+      git
+      (ripgrep.override { withPCRE2 = true; })
+      gnutls # for TLS connectivity
 
-  programs.zsh = mkIf opts.zsh-integration {
-    envExtra = ''
-      # Emacs
-      path+=("$HOME/.emacs.d/bin")
+      ## Optional dependencies
+      fd # faster projectile indexing
+      imagemagick # for image-dired
+      # (mkIf (config.programs.gnupg.agent.enable)
+      #   pinentry_emacs) # in-emacs gnupg prompts
+      zstd # for undo-fu-session/undo-tree compression
+
+      ## Module dependencies
+      # :checkers spell
+      (hunspellWithDicts (with pkgs.hunspellDicts; [
+        en-us
+        ru_RU
+        en_GB-ize
+        en_US-large
+      ]))
+      # :tools editorconfig
+      editorconfig-core-c # per-project style config
+      # :tools lookup & :lang org +roam
+      sqlite
+      # :lang latex & :lang org (latex previews)
+      texlive.combined.scheme-medium
+      # :lang org & graphs
+      graphviz
+      # :lang web
+      html-tidy
+      jsbeautifier
+      nodePackages_latest.stylelint
+      # :lang org & export with pandoc
+      pandoc
+      # :lang go
+      gomodifytags
+      gotests
+      gore
+      gotools
+      # :lang nix
+      nixfmt
+      # :lang beancount
+      beancount
+      fava
+
+      # Fonts
+      emacs-all-the-icons-fonts
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    ];
+
+    home.activation.emacs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      PATH=$PATH:${lib.makeBinPath [ pkgs.git ]}.
+      if [ ! -d "$HOME/.config/emacs" ]; then
+          ${pkgs.git}/bin/git clone --depth=1 --single-branch "${repoUrl}" "$HOME/.config/emacs"
+          $DRY_RUN_CMD ln -sfn $VERBOSE_ARG \
+              "${opts.realConfigs}/doom" "$HOME/.config/doom"
+      fi
     '';
+
+    programs.zsh = {
+      envExtra = ''
+        path+=("$HOME/.config/emacs/bin/")
+      '';
+    };
   };
-
-  fonts.fontconfig.enable = true;
-
-  home.packages = with pkgs; [
-    ## Emacs itself
-    binutils # native-comp needs 'as', provided by this
-    # 28.2 + native-comp
-    ((emacsPackagesFor emacs).emacsWithPackages (epkgs: [
-      epkgs.vterm
-    ]))
-
-    ## Doom dependencies
-    git
-    (ripgrep.override { withPCRE2 = true; })
-    gnutls # for TLS connectivity
-
-    ## Optional dependencies
-    fd # faster projectile indexing
-    imagemagick # for image-dired
-    zstd # for undo-fu-session/undo-tree compression
-
-    # hunspell
-    (hunspellWithDicts (with pkgs.hunspellDicts; [
-      en-us
-      ru_RU
-      en_GB-ize
-      en_US-large
-    ]))
-
-    ## Module dependencies
-    # :checkers spell
-    # (aspellWithDicts (ds: with ds; [ en en-computers en-science ru ]))
-    # :tools editorconfig
-    editorconfig-core-c # per-project style config
-    # :tools lookup & :lang org +roam
-    sqlite
-    # :lang latex & :lang org (latex previews)
-    # texlive.combined.scheme-medium
-    # :lang beancount
-    # beancount
-    # fava # HACK Momentarily broken on nixos-unstable
-
-    shellcheck
-
-    # Nix
-    nixfmt
-
-    # for emacs-lisp-autofmt
-    python3
-
-    # fcitx5-with-addons
-
-    emacs-all-the-icons-fonts
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-  ];
 }
 
