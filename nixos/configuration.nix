3c2a789662d7aa7747f076f1c43de4ac5769846b
Sleroq <hireme@sleroq.link>
2023-06-17 18:22:05 +0300

    Battery life improvements

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -97,6 +97,20 @@
     xkbVariant = "";
   };
 
+  # Battery life
+  services.power-profiles-daemon.enable = false;
+  services.thermald.enable = true;
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
