9d4ac3f2526877e0dc5e0ad3875a2f5ebbafa20c
Sleroq <hireme@sleroq.link>
2023-10-24 22:54:30 +0300

    Changed ltp settings to use less power

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -114,10 +114,10 @@
   services.tlp = {
     enable = true;
     settings = {
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      RUNTIME_PM_ON_AC="auto";
+      RUNTIME_PM_ON_BAT="auto";
+      CPU_ENERGY_PERF_POLICY_ON_AC="balance_power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT="balance_power";
     };
   };
 
