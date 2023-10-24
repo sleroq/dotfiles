fcf5d1e4c94513a3ea509857e2e8cbeeeeff94bd
Sleroq <hireme@sleroq.link>
2023-10-24 22:52:03 +0300

    Added workaround for opening links from flatpak

--- nixos/modules/flatpak-workaround.nix
+++ nixos/modules/flatpak-workaround.nix
@@ -20,4 +20,10 @@
       "/usr/share/icons" = mkRoSymBind (config.system.path + "/share/icons");
       "/usr/share/fonts" = mkRoSymBind (aggregatedFonts + "/share/fonts");
     };
+
+    # Fix for https://discourse.nixos.org/t/clicked-links-in-desktop-apps-not-opening-browers/29114/26
+    # From this comment https://github.com/NixOS/nixpkgs/issues/189851#issuecomment-1759954096
+    systemd.user.extraConfig = ''
+      DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+    '';
 }
