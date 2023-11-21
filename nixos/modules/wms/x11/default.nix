03b0387c729ae117cedb9e046f7808af84f440b6
Sleroq <hireme@sleroq.link>
2023-11-21 21:22:15 +0300

    Updated x11 configuration

--- nixos/modules/wms/x11/default.nix
+++ nixos/modules/wms/x11/default.nix
@@ -1,12 +1,6 @@
 { pkgs, ... }: {
   services.xserver.excludePackages = [ pkgs.xterm ];
 
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    windowManager.leftwm.enable = true;
-  };
-
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
--- nixos/modules/wms/x11/leftwm.nix
+++ nixos/modules/wms/x11/leftwm.nix
@@ -1,4 +1,9 @@
 { pkgs, ... }:
 {
+  services.xserver = {
+    enable = true;
+    windowManager.leftwm.enable = true;
+  };
+
   programs.slock.enable = true;
 }
