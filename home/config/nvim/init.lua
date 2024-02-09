8adfb845162ebbacf439a7f8162427036adefe22
Sleroq <hireme@sleroq.link>
2024-02-09 23:24:28 +0300

    Configured lia-line

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -175,9 +175,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'auto',
-        component_separators = '|',
-        section_separators = '',
+        theme = 'palenight',
       },
     },
   },
