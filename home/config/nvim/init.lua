5c4da39f629908e3726fc08e2cad04a4ea137b12
Sleroq <hireme@sleroq.link>
2024-02-09 23:26:01 +0300

    Added remote-neovim

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -260,6 +260,16 @@ require('lazy').setup({
       })
     end,
   },
+  {
+     "amitds1997/remote-nvim.nvim",
+     version = "*", -- Pin to GitHub releases
+     dependencies = {
+         "nvim-lua/plenary.nvim", -- For standard functions
+         "MunifTanjim/nui.nvim", -- To build the plugin UI
+         "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+     },
+     config = true,
+  },
 }, {})
 
 -- [[ Setting options ]]
