7afe91c4903d467ecc90420b05fc6c218b3a82cb
Sleroq <hireme@sleroq.link>
2024-02-21 23:40:23 +0300

    Enabled terminal transparancy

--- home/modules/programs/foot.nix
+++ home/modules/programs/foot.nix
@@ -21,6 +21,7 @@
 
       # Catpussy machiatto
       colors = {
+        alpha = "0.9";
         foreground = "cad3f5"; # Text
         background = "24273a"; # Base
         regular0 = "494d64";   # Surface 1
@@ -40,28 +41,6 @@
         bright6 = "8bd5ca";    # teal
         bright7 = "a5adcb";    # Subtext 0
       };
-
-     # Catpussy mocha
-     # colors = {
-     #   foreground = "cdd6f4"; # Text
-     #   background = "1e1e2e"; # Base
-     #   regular0 = "45475a";   # Surface 1
-     #   regular1 = "f38ba8";   # red
-     #   regular2 = "a6e3a1";   # green
-     #   regular3 = "f9e2af";   # yellow
-     #   regular4 = "89b4fa";   # blue
-     #   regular5 = "f5c2e7";   # pink
-     #   regular6 = "94e2d5";   # teal
-     #   regular7 = "bac2de";   # Subtext 1
-     #   bright0 = "585b70";    # Surface 2
-     #   bright1 = "f38ba8";    # red
-     #   bright2 = "a6e3a1";    # green
-     #   bright3 = "f9e2af";    # yellow
-     #   bright4 = "89b4fa";    # blue
-     #   bright5 = "f5c2e7";    # pink
-     #   bright6 = "94e2d5";    # teal
-     #   bright7 = "a6adc8";    # Subtext 0
-     # };
     };
   };
 
