ece688853dc7a0a5106085fe57ea30a45a2dbb14
Sleroq <hireme@sleroq.link>
2024-03-16 18:52:32 +0300

    Added some configurations

---
+++ nixos/modules/kwallet.nix
@@ -0,0 +1,13 @@
+
+
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    libsForQt5.kwallet
+    libsForQt5.kwalletmanager
+  ];
+
+  security.pam.services.kwallet = {
+    name = "kwallet";
+    enableKwallet = true;
+  };
+}
