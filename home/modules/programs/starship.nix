d5308ec0cf0cd7779726d5fc9624959c317a756f
Sleroq <hireme@sleroq.link>
2024-05-28 22:02:48 +0300

    Added starship configuration

---
+++ home/modules/programs/starship.nix
@@ -0,0 +1,37 @@
+{ lib, ... }:
+
+{
+  programs.starship = {
+    enable = true;
+    enableNushellIntegration = true;
+    settings = {
+      add_newline = false;
+
+      format = lib.concatStrings [
+        "$line_break"
+        "$all"
+
+        "$fill"
+
+        "$time"
+        "$line_break"
+        "$jobs"
+        "$battery"
+        "$status"
+        "$os"
+        "$container"
+        "$shell"
+        "$character"
+      ];
+
+      time = {
+        disabled = false;
+        use_12hr = true;
+        time_format = "%T %p";
+      };
+      fill = {
+        symbol = " ";
+      };
+    };
+  };
+}
