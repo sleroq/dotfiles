41cb254691ddb9bb4a750f07941d6c44f7cf3c98
Sleroq <hireme@sleroq.link>
2024-05-29 08:06:58 +0300

    Removed some unused steam options

--- nixos/modules/apps.nix
+++ nixos/modules/apps.nix
@@ -28,8 +28,6 @@
   programs.java.enable = true;
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     gamescopeSession.enable = true;
   };
 
