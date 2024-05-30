0bc0b6beea1d3da345c90a4bd9674f3ff9fcbdbe
Sleroq <hireme@sleroq.link>
2024-05-31 00:15:14 +0300

    Fixed something

--- home/config/nvim/after/plugin/nvim-tree.lua
+++ home/config/nvim/after/plugin/nvim-tree.lua
@@ -1,15 +1,19 @@
--- init.lua
+require('nvim-tree').setup {
+  git = {
+    ignore = false,
+  },
+}
+
 local nvimTreeFocusOrToggle = function()
-  local nvimTree = require("nvim-tree.api")
+  local nvimTree = require 'nvim-tree.api'
   local currentBuf = vim.api.nvim_get_current_buf()
-  local currentBufFt = vim.api.nvim_get_option_value("filetype", { buf = currentBuf })
-  if currentBufFt == "NvimTree" then
+  local currentBufFt = vim.api.nvim_get_option_value('filetype', { buf = currentBuf })
+  if currentBufFt == 'NvimTree' then
     nvimTree.tree.toggle()
   else
     nvimTree.tree.focus()
   end
 end
-
 
 vim.keymap.set('n', '<leader>ff', nvimTreeFocusOrToggle, { desc = '[F]ile browser' })
 vim.keymap.set('n', '<leader>op', nvimTreeFocusOrToggle, { desc = '[F]ile browser' })
