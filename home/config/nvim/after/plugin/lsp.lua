5e1b6099bfb4f060c3c25390db01b8c7d21c3ffa
Sleroq <hireme@sleroq.link>
2024-06-02 16:35:41 +0300

    Fixed warnings, removed duplicate configs

--- home/config/nvim/after/plugin/lsp.lua
+++ home/config/nvim/after/plugin/lsp.lua
@@ -4,58 +4,17 @@ lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
     lsp_zero.default_keymaps { buffer = bufnr }
-
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = 'LSP: ' .. desc
-        end
-
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-    end
-
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', function()
-        vim.lsp.buf.code_action { context = { only = { 'quickfix', 'refactor', 'source' } } }
-    end, '[C]ode [A]ction')
-
-    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-    -- See `:help K` for why this keymap
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-    -- Lesser used LSP functionality
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    -- nmap('<leader>wl', function()
-    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, '[W]orkspace [L]ist Folders')
 end)
 
--- technically these are "diagnostic signs"
--- neovim enables them by default.
--- here we are just changing them to fancy icons.
-lsp_zero.set_sign_icons {
-    error = '✘',
-    warn = '▲',
-    hint = '⚑',
-    info = '»',
-}
-
 require('mason').setup {}
--- require 'mason-lspconfig'
---
--- require('mason-lspconfig').setup {
---   handlers = { lsp_zero.default_setup },
--- }
-
-vim.lsp.set_log_level 'debug'
+require('mason-lspconfig').setup {
+    ensure_installed = {},
+    handlers = {
+        function(server_name)
+            require('lspconfig')[server_name].setup {}
+        end,
+    },
+}
 
 local nvim_lsp = require 'lspconfig'
 
@@ -63,6 +22,7 @@ nvim_lsp.lua_ls.setup {}
 nvim_lsp.nixd.setup {}
 nvim_lsp.denols.setup {}
 nvim_lsp.gopls.setup {}
+nvim_lsp.golangci_lint_ls.setup {}
 
 local cmp = require 'cmp'
 local cmp_action = lsp_zero.cmp_action()
@@ -107,7 +67,7 @@ cmp.setup {
     },
     -- note: if you are going to use lsp-kind (another plugin)
     -- replace the line below with the function from lsp-kind
-    formatting = lsp_zero.cmp_format(),
+    formatting = lsp_zero.cmp_format {},
 }
 
 -- Ufo
@@ -122,19 +82,37 @@ lsp_zero.set_server_config {
     },
 }
 
-lsp_zero.preset 'recommended'
-
-lsp_zero.configure('tsserver', {
-    single_file_support = false,
-    autostart = false,
-    root_dir = require('lspconfig.util').root_pattern 'package.json',
+lsp_zero.configure('denols', {
+    root_dir = nvim_lsp.util.root_pattern("deno.json"),
+    init_options = {
+        lint = true,
+        unstable = true,
+        suggest = {
+            imports = {
+                hosts = {
+                    ["https://deno.land"] = true,
+                    ["https://cdn.nest.land"] = true,
+                    ["https://crux.land"] = true,
+                },
+            },
+        },
+    },
+    on_attach = function()
+        local active_clients = vim.lsp.get_clients()
+        for _, client in pairs(active_clients) do
+            -- stop tsserver if denols is already active
+            if client.name == "tsserver" then
+                client.stop()
+            end
+        end
+    end,
 })
 
 lsp_zero.configure('beancount', {
     init_options = {
         journal_file = '~/Sync/Shared org/beancount/kek.beancount',
     },
-    autostart = true
-});
+    autostart = true,
+})
 
 lsp_zero.setup()
