c8b38f471fa5c2b902e9a1e7556321f7cba6306e
Sleroq <hireme@sleroq.link>
2024-06-15 17:36:35 +0300

    Removed waybar configuration

--- home/overlays.nix
+++ home/overlays.nix
@@ -6,12 +6,4 @@ let
 in
 self: super: {
   obinskit = super.callPackage ./packages/obinskit.nix { };
-
-  waybar-hyprland = super.waybar.overrideAttrs (oldAttrs: {
-    postPatch = ''
-      # use hyprctl to switch workspaces
-      sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
-    '';
-    mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-  });
 }
