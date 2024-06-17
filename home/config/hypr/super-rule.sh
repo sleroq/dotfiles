24e4dd98e682561e2fe157e5491911c816d3d108
Sleroq <hireme@sleroq.link>
2024-06-17 21:27:02 +0300

    Fixed autofocus

--- home/config/hypr/super-rule.sh
+++ home/config/hypr/super-rule.sh
@@ -16,19 +16,16 @@ handle() {
       pcmanfm_pid=$(pgrep pcmanfm-qt)
 
       if [[ "$window_pid" == "$pcmanfm_pid" ]]; then
-        hyprctl --batch "dispatch togglefloating address:0x$window_id; dispatch resizewindowpixel exact 60% 60%,address:0x$window_id; dispatch movewindowpixel exact 20% 20%,address:0x$window_id"
+        hyprctl --batch "dispatch togglefloating address:0x$window_id; dispatch resizewindowpixel exact 60% 60%,address:0x$window_id; dispatch movewindowpixel exact 20% 20%,address:0x$window_id" &
       fi
+      ;;
 
-      # Check if the title matches the characteristics of the Bitwarden popup window
-      # if [[ "$window_title" == *"(Bitwarden - Free Password Manager) - Bitwarden"* ]]; then
-      # 
-      #   # echo $window_id, $window_title
-      #   # hyprctl dispatch togglefloating address:0x$window_id
-      #   # hyprctl dispatch resizewindowpixel exact 20% 40%,address:0x$window_id
-      #   # hyprctl dispatch movewindowpixel exact 40% 30%,address:0x$window_id
-      #
-      #   hyprctl --batch "dispatch togglefloating address:0x$window_id ; dispatch resizewindowpixel exact 20% 40%,address:0x$window_id ; dispatch movewindowpixel exact 40% 30%,address:0x$window_id"        
-      # fi
+    movewindowv2*)
+      # Extract the window ID and the new position from the line
+      window_id=$(echo ${1#*>>} | cut -d, -f1)
+
+      # Always focus window when it's being moved to other workspace
+      hyprctl dispatch focuswindow address:0x$window_id &
       ;;
   esac
 }
