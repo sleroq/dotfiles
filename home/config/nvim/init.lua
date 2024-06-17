57a4c180cf0b3da482fa2e78e7c7725daa9075e4
Sleroq <hireme@sleroq.link>
2024-06-17 09:13:25 +0300

    Improved indent detection

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -1,210 +1,217 @@
 -- Set <space> as the leader key
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- [[ :help lazy.nvim.txt ]]
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system {
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable',
-        lazypath,
-    }
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure plugins ]]
-require('lazy').setup({
-    'nvim-tree/nvim-web-devicons',
+require("lazy").setup({
+	"nvim-tree/nvim-web-devicons",
 
-    -- Git related plugins
-    'tpope/vim-fugitive',
+	-- Git related plugins
+	"tpope/vim-fugitive",
 
-    -- Detect tabstop and shiftwidth automatically
-    -- 'tpope/vim-sleuth',
+	-- Detect tabstop and shiftwidth automatically
+	-- 'tpope/vim-sleuth',
 
-    -- LSP related plugins
-    'neovim/nvim-lspconfig',
-    'hrsh7th/cmp-buffer',
-    opts = {},
-    'hrsh7th/cmp-path',
-    'saadparwaiz1/cmp_luasnip',
-    'hrsh7th/cmp-nvim-lua',
-    'rafamadriz/friendly-snippets',
-    { 'L3MON4D3/LuaSnip',                  opts = {} },
-    { 'williamboman/mason.nvim',           opts = {} },
-    { 'williamboman/mason-lspconfig.nvim', opts = {} },
-    { 'hrsh7th/nvim-cmp',                  opts = {} },
-    { 'hrsh7th/cmp-nvim-lsp',              opts = {} },
-    { 'VonHeikemen/lsp-zero.nvim',         branch = 'v3.x', lazy = true, config = false },
+	-- LSP related plugins
+	"neovim/nvim-lspconfig",
+	"hrsh7th/cmp-buffer",
+	opts = {},
+	"hrsh7th/cmp-path",
+	"saadparwaiz1/cmp_luasnip",
+	"hrsh7th/cmp-nvim-lua",
+	"rafamadriz/friendly-snippets",
+	{ "L3MON4D3/LuaSnip",                  opts = {} },
+	{ "williamboman/mason.nvim",           opts = {} },
+	{ "williamboman/mason-lspconfig.nvim", opts = {} },
+	{ "hrsh7th/nvim-cmp",                  opts = {} },
+	{ "hrsh7th/cmp-nvim-lsp",              opts = {} },
+	{ "VonHeikemen/lsp-zero.nvim",         branch = "v3.x", lazy = true, config = false },
 
-    {
-        'nvimdev/guard.nvim',
-        -- Builtin configuration, optional
-        dependencies = {
-            'nvimdev/guard-collection',
-        },
-    },
+	{
+		"nvimdev/guard.nvim",
+		-- Builtin configuration, optional
+		dependencies = {
+			"nvimdev/guard-collection",
+		},
+	},
 
-    { 'j-hui/fidget.nvim',    opts = {} },
-    { 'folke/neodev.nvim',    opts = {} },
-    { 'folke/which-key.nvim', opts = {} },
-    'lewis6991/gitsigns.nvim',
+	{ "j-hui/fidget.nvim",    opts = {} },
+	{ "folke/neodev.nvim",    opts = {} },
+	{ "folke/which-key.nvim", opts = {} },
+	"lewis6991/gitsigns.nvim",
 
-    {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        priority = 1000,
-        lazy = false,
-        config = function()
-            require('catppuccin').setup {
-                transparent_background = true,
-                integrations = {
-                    cmp = true,
-                    gitsigns = true,
-                    treesitter = true,
-                    telescope = true,
-                },
-            }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					treesitter = true,
+					telescope = true,
+				},
+			})
 
-            vim.cmd.colorscheme 'catppuccin-macchiato'
-        end,
-    },
+			vim.cmd.colorscheme("catppuccin-macchiato")
+		end,
+	},
 
-    {
-        -- Set lualine as statusline
-        'nvim-lualine/lualine.nvim',
-        -- See `:help lualine.txt`
-        opts = {
-            options = {
-                icons_enabled = false,
-                theme = 'palenight',
-            },
-        },
-    },
+	{
+		-- Set lualine as statusline
+		"nvim-lualine/lualine.nvim",
+		-- See `:help lualine.txt`
+		opts = {
+			options = {
+				icons_enabled = false,
+				theme = "palenight",
+			},
+		},
+	},
 
-    {
-        -- Add indentation guides even on blank lines
-        'lukas-reineke/indent-blankline.nvim',
-        -- Enable `lukas-reineke/indent-blankline.nvim`
-        -- See `:help ibl`
-        main = 'ibl',
-        opts = {},
-    },
+	{
+		-- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		opts = {},
+	},
 
-    -- 'gc' to comment visual regions/lines
-    { 'numToStr/Comment.nvim',  opts = {} },
+	-- 'gc' to comment visual regions/lines
+	{ "numToStr/Comment.nvim",  opts = {} },
 
-    -- Fuzzy Finder (files, lsp, etc)
-    {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-            -- Only load if `make` is available. Make sure you have the system
-            -- requirements installed.
-            {
-                'nvim-telescope/telescope-fzf-native.nvim',
-                -- NOTE: If you are having trouble with this installation,
-                --       refer to the README for telescope-fzf-native for more instructions.
-                build = 'make',
-                cond = function()
-                    return vim.fn.executable 'make' == 1
-                end,
-            },
-        },
-    },
+	-- Fuzzy Finder (files, lsp, etc)
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
+			-- Only load if `make` is available. Make sure you have the system
+			-- requirements installed.
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				-- NOTE: If you are having trouble with this installation,
+				--       refer to the README for telescope-fzf-native for more instructions.
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+		},
+	},
 
-    {
-        -- Highlight, edit, and navigate code
-        'nvim-treesitter/nvim-treesitter',
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
-        },
-        build = ':TSUpdate',
-    },
-    {
-        'zbirenbaum/copilot.lua',
-        config = function()
-            require('copilot').setup {
-                suggestion = {
-                    enabled = true,
-                    auto_trigger = true,
-                    debounce = 75,
-                    keymap = {
-                        accept = '<M-l>',
-                        accept_word = false,
-                        accept_line = false,
-                        next = '<M-]>',
-                        prev = '<M-[>',
-                        dismiss = '<C-]>',
-                    },
-                },
-            }
-        end,
-    },
-    'zbirenbaum/copilot-cmp',
+	{
+		-- Highlight, edit, and navigate code
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		build = ":TSUpdate",
+	},
+	{
+		"zbirenbaum/copilot.lua",
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					debounce = 75,
+					keymap = {
+						accept = "<M-l>",
+						accept_word = false,
+						accept_line = false,
+						next = "<M-]>",
+						prev = "<M-[>",
+						dismiss = "<C-]>",
+					},
+				},
+			})
+		end,
+	},
+	"zbirenbaum/copilot-cmp",
 
-    {
-        'amitds1997/remote-nvim.nvim',
-        version = '*',                       -- Pin to GitHub releases
-        dependencies = {
-            'nvim-lua/plenary.nvim',         -- For standard functions
-            'MunifTanjim/nui.nvim',          -- To build the plugin UI
-            'nvim-telescope/telescope.nvim', -- For picking b/w different remote methods
-        },
-        config = true,
-    },
+	{
+		"amitds1997/remote-nvim.nvim",
+		version = "*",      -- Pin to GitHub releases
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- For standard functions
+			"MunifTanjim/nui.nvim", -- To build the plugin UI
+			"nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+		},
+		config = true,
+	},
 
-    'folke/zen-mode.nvim',
-    { 'echasnovski/mini.pairs', version = false, opts = {} },
+	"folke/zen-mode.nvim",
+	{ "echasnovski/mini.pairs", version = false, opts = {} },
 
-    {
-        'kevinhwang91/nvim-ufo',
-        setup = function()
-            -- Using ufo provider need remap `zR` and `zM`.
-            vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-            vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-        end,
-    },
+	{
+		"kevinhwang91/nvim-ufo",
+		setup = function()
+			-- Using ufo provider need remap `zR` and `zM`.
+			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+		end,
+	},
 
-    {
-        'nvim-tree/nvim-tree.lua',
-        version = '*',
-        lazy = false,
-        dependencies = {
-            'nvim-tree/nvim-web-devicons',
-        },
-        config = function()
-            require('nvim-tree').setup {}
-        end,
-    },
-    'ledger/vim-ledger',
-    'nathangrigg/vim-beancount',
-    'benfowler/telescope-luasnip.nvim',
-    'nvim-telescope/telescope-frecency.nvim',
-    {
-        'LhKipp/nvim-nu',
-        config = function()
-            require('nvim-tree').setup {}
-        end,
-    },
-    {
-        'folke/todo-comments.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        opts = {},
-    },
-    {
-        'theRealCarneiro/hyprland-vim-syntax',
-        dependencies = { 'nvim-treesitter/nvim-treesitter' },
-        ft = 'hypr'
-    },
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	},
+	"ledger/vim-ledger",
+	"nathangrigg/vim-beancount",
+	"benfowler/telescope-luasnip.nvim",
+	"nvim-telescope/telescope-frecency.nvim",
+	{
+		"LhKipp/nvim-nu",
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+	{
+		"theRealCarneiro/hyprland-vim-syntax",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		ft = "hypr",
+	},
+	{
+
+		"nmac427/guess-indent.nvim",
+		config = function()
+			require("guess-indent").setup({})
+		end,
+	}
 }, {})
 
 -- [[ Setting options ]]
-require 'sleroq'
+require("sleroq")
--- home/config/nvim/lua/sleroq/options.lua
+++ home/config/nvim/lua/sleroq/options.lua
@@ -7,12 +7,12 @@ vim.o.hlsearch = false
 vim.wo.number = true
 
 -- Enable mouse mode
-vim.o.mouse = "a"
+vim.o.mouse = 'a'
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = "unnamedplus"
+vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -25,20 +25,20 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = "yes"
+vim.wo.signcolumn = 'yes'
 
 -- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone,noselect"
+vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
-vim.o.foldmethod = "indent"
-vim.o.foldcolumn = "1"
+vim.o.foldmethod = 'indent'
+vim.o.foldcolumn = '1'
 vim.o.foldlevel = 999
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
@@ -54,21 +54,21 @@ vim.opt.smartindent = true
 -- Disable wrap
 vim.o.wrap = false
 
-vim.o.guifont = "JetBrainsMono Nerd Font:h18"
+vim.o.guifont = 'JetBrainsMono Nerd Font:h18'
 vim.opt.linespace = 4
 vim.g.neovide_transparency = 0.8
-vim.g.neovide_cursor_vfx_mode = "pixiedust"
+vim.g.neovide_cursor_vfx_mode = 'pixiedust'
 vim.g.neovide_cursor_vfx_particle_density = 14.0
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = "*",
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = '*',
 })
 
 -- Netrw
