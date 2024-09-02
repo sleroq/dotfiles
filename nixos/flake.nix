485fc8c364513eb45781e933b04cd80f7ab1cde3
Sleroq <hireme@sleroq.link>
2024-09-02 20:49:27 +0300

    Added unstable overlay

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,6 +2,7 @@
   description = "Sleroq's NixOS system configuration";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.hyprland = {
     type = "git";
     url = "https://github.com/hyprwm/Hyprland";
@@ -9,10 +10,16 @@
     ref = "refs/tags/v0.42.0";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
+    overlay-unstable = final: prev: {
+      unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    };
   in {
     overlays = import ./overlays { inherit inputs; inherit (self.outputs) outputs; };
     packages = import ./pkgs { inherit pkgs; inherit (nixpkgs.lib) lib; };
@@ -20,7 +27,10 @@
     nixosConfigurations.sleroq-international = nixpkgs.lib.nixosSystem {
       inherit system;
       specialArgs = { inherit inputs; inherit (self) outputs; };
-      modules = [ ./configuration.nix ];
+      modules = [
+        ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+        ./configuration.nix
+      ];
     };
   };
 }
