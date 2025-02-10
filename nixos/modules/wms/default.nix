9e4b31508a930e8d5ab567fd33da3136d1b48df9
Sleroq <hireme@sleroq.link>
2025-02-10 21:43:10 +0300

    Removed dwl

--- nixos/modules/wms/default.nix
+++ nixos/modules/wms/default.nix
@@ -6,12 +6,12 @@
 
   environment.systemPackages = [
     pkgs.xdg-utils
-    (pkgs.unstable.where-is-my-sddm-theme.override { variants = [ "qt5" ]; })
+    pkgs.where-is-my-sddm-theme
   ];
 
   services.displayManager.sddm = {
     enable = true;
-    package = pkgs.libsForQt5.sddm;
-    theme = "where_is_my_sddm_theme_qt5";
+    package = pkgs.kdePackages.sddm;
+    theme = "where_is_my_sddm_theme";
   };
 }
