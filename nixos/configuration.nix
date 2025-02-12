083e140ef1101be0ba9502ade992e46543cd775e
Sleroq <hireme@sleroq.link>
2025-02-12 20:01:01 +0300

    Replaced lxqt polkit with hypr polkit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,7 +7,6 @@
 
     # ./modules/samba.nix
     ./modules/flatpak.nix
-    ./modules/polkit.nix
     ./modules/kwallet.nix
     ./modules/virtualisation.nix
     ./modules/wms/default.nix
@@ -172,6 +171,8 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
+
+  security.polkit.enable = true;
 
   # Enable flakes:
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
