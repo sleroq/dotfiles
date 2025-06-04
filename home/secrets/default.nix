1faa4a8d341f8703bf017e546a8ce7b6c5697d8e
Sleroq <hireme@sleroq.link>
2025-06-04 06:47:44 +0300

    Added some more docs

--- home/secrets/default.nix
+++ home/secrets/default.nix
@@ -1,6 +1,8 @@
 # This file contains some stuff I don't really want to commit on github,
 # but it's for it to end up in the nix store
 # see https://github.com/NixOS/nix/issues/7107
+# Use `git update-index --no-skip-worktree ./secrets/default.nix`
+# to ignore the file locally, so you don't commit it by accident
 
 {
   wezterm-ssh-domains = "";
--- nixos/hosts/cumserver/secrets/default.nix
+++ nixos/hosts/cumserver/secrets/default.nix
@@ -1,5 +1,7 @@
 # This file contains some stuff I don't really want to commit on github,
 # but it's for it to end up in the nix store
+# Use `git update-index --skip-worktree ./secrets/default.nix`
+# to ignore the file locally, so you don't commit it by accident
 # see https://github.com/NixOS/nix/issues/7107
 
 {
