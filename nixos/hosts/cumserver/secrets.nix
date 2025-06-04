dba331813452a0cb69343d0f07189bea2a230674
Sleroq <hireme@sleroq.link>
2025-06-04 06:16:30 +0300

    Refactored secrets.nix file

--- nixos/hosts/cumserver/secrets.nix
+++ nixos/hosts/cumserver/secrets.nix
@@ -1,7 +1,34 @@
 let
   sleroq = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK72BBTnP5Os5ZQfS1BuigNzWMqNFl7lgUH4CJq1bl9P cantundo@pm.me";
   cumserver = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGNy4lHOvczy/vR4hf+uk6ciJGpkw5mqu3oC+9hTDbqf";
+  commonPublicKeys = [ sleroq cumserver ];
+
+  numberOfPasswords = 4;
+  mailSecretBasePath = "secrets/mail/";
+  passwordNamePrefix = "password";
+
+  passwordNumbers = builtins.genList
+    (index: index + 1)
+    numberOfPasswords;
+
+  passwordFilePaths = builtins.map
+    (n: "${mailSecretBasePath}${passwordNamePrefix}${builtins.toString n}")
+    passwordNumbers;
+
+  generatedPasswordEntries = builtins.listToAttrs (
+    builtins.map
+      (filePath: {
+        name = filePath;
+        value = { publicKeys = commonPublicKeys; };
+      })
+      passwordFilePaths
+  );
+
+  staticEntries = {
+    "secrets/matterbridge.toml" = { publicKeys = commonPublicKeys; };
+    "secrets/cf-fullchain.pem" = { publicKeys = commonPublicKeys; };
+    "secrets/cf-privkey.pem" = { publicKeys = commonPublicKeys; };
+  };
+
 in
-{
-  "secrets/matterbridge.toml".publicKeys = [ sleroq cumserver ];
-}
+  staticEntries // generatedPasswordEntries
