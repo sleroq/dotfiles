f2969278eaa7b48ab8fd83d90fec15d48b8bfb70
Sleroq <hireme@sleroq.link>
2025-06-22 19:37:12 +0300

    Added zed cache

--- nixos/hosts/shared/default.nix
+++ nixos/hosts/shared/default.nix
@@ -99,11 +99,19 @@
       "https://nix-community.cachix.org"
       "https://cache.nixos.org/"
       "https://hyprland.cachix.org"
+
+      # From zed flake
+      "https://cache.garnix.io"
+      "https://zed.cachix.org"
     ];
     trusted-public-keys = [
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+
+      # From zed flake
+      "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 
