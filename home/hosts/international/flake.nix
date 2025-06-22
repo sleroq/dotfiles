d4f4ad6e83e6297cecc0d33161ed3d602d4d5d87
Sleroq <hireme@sleroq.link>
2025-06-22 19:42:55 +0300

    Copied all inputs to the laptop flake

--- home/hosts/international/flake.nix
+++ home/hosts/international/flake.nix
@@ -2,18 +2,25 @@
   description = "Home Manager configuration of Sleroq";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # For old packages, like obinskit
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    scrcpyPkgs.url = "github:nixos/nixpkgs/77a0bdd";
     nixpkgs-old.url = "github:nixos/nixpkgs/1c37a89390481e809b9851781026bc9bb840dd90";
+    nixpkgs-master.url = "github:nixos/nixpkgs/master";
+    secrets = {
+      flake = false;
+      url = "path:../../secrets";
+    };
 
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-gaming.url = "github:fufexan/nix-gaming";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     # paisa.url = "github:ananthakumaran/paisa";
     zig.url = "github:mitchellh/zig-overlay";
+    zls.url = "github:zigtools/zls";
+    zed.url = "github:zed-industries/zed?submodules=1&ref=refs/tags/v0.192.2-pre";
+    agenix.url = "github:ryantm/agenix";
 
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref=refs/tags/v0.49.0";
 
