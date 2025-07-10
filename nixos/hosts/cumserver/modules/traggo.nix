53a09643e3d06b5c4749a38ba64e20d990e6de67
Sleroq <hireme@sleroq.link>
2025-07-10 21:35:17 +0300

    Added traggo

---
+++ nixos/hosts/cumserver/modules/traggo.nix
@@ -0,0 +1,83 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.cumserver.traggo;
+in
+{
+  options.cumserver.traggo = {
+    enable = lib.mkEnableOption "Traggo time tracking application";
+
+    image = lib.mkOption {
+      type = lib.types.str;
+      default = "traggo/server:latest";
+      description = "Docker image to use for Traggo";
+    };
+
+    domain = lib.mkOption {
+      type = lib.types.str;
+      default = "time.cum.army";
+      description = "Domain name for Traggo";
+    };
+
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 3030;
+      description = "Internal port for Traggo";
+    };
+
+    dataDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/var/lib/traggo";
+      description = "Data directory for Traggo";
+    };
+
+    environment = lib.mkOption {
+      type = lib.types.attrs;
+      default = {};
+      description = "Environment variables for Traggo";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = config.virtualisation.oci-containers.backend != null;
+        message = "OCI containers backend must be configured for Traggo to work";
+      }
+      {
+        assertion = config.services.caddy.enable;
+        message = "Caddy has to be enabled for Traggo to work";
+      }
+    ];
+
+    virtualisation.oci-containers.containers.traggo = {
+      autoStart = true;
+      image = cfg.image;
+      environment = cfg.environment;
+      ports = [ "127.0.0.1:${toString cfg.port}:3030" ];
+      volumes = [
+        "${cfg.dataDir}:/opt/traggo/data"
+      ];
+    };
+
+    services.caddy.virtualHosts.${cfg.domain} = {
+      extraConfig = ''
+        reverse_proxy 127.0.0.1:${toString cfg.port}
+        
+        header {
+          X-Content-Type-Options nosniff
+          X-Frame-Options SAMEORIGIN
+          X-XSS-Protection "1; mode=block"
+          X-Real-IP {remote_addr}
+          X-Forwarded-For {remote_addr}
+          X-Forwarded-Host {host}
+        }
+        
+        encode zstd gzip
+      '';
+    };
+
+    systemd.tmpfiles.rules = [
+      "d ${cfg.dataDir} 0755 root root -"
+    ];
+  };
+} 
