42fc9f0926cb3c2b92a9eaf06b7f356582fc05ae
Sleroq <hireme@sleroq.link>
2025-08-03 13:10:32 +0300

    Added configuration option for experimental profiles feature

--- nixos/hosts/cumserver/modules/broadcast-box.nix
+++ nixos/hosts/cumserver/modules/broadcast-box.nix
@@ -45,6 +45,42 @@ in
         NETWORK_TEST_ON_START = "false";
       };
     };
+    
+    profiles = {
+      enable = lib.mkEnableOption "Stream profiles support";
+      
+      streamProfiles = lib.mkOption {
+        type = lib.types.attrsOf (lib.types.submodule {
+          options = {
+            streamKey = lib.mkOption {
+              type = lib.types.str;
+              description = "Stream key for the profile";
+            };
+            
+            isPublic = lib.mkOption {
+              type = lib.types.bool;
+              default = true;
+              description = "Whether the stream is public";
+            };
+            
+            motd = lib.mkOption {
+              type = lib.types.str;
+              default = "Welcome to the stream!";
+              description = "Message of the day for the stream";
+            };
+          };
+        });
+        default = {};
+        description = "Stream profiles configuration";
+        example = {
+          "saygex2_1b2e45eb-360c-4d75-a29f-0ecff7e88762" = {
+            streamKey = "saygex2";
+            isPublic = true;
+            motd = "Welcome to my stream!";
+          };
+        };
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -59,7 +95,25 @@ in
       }
     ];
 
-    # Open UDP port in firewall for WebRTC traffic
+    environment.etc = lib.mkIf cfg.profiles.enable (
+      lib.mapAttrs' (name: profile: {
+        name = "broadcast-box/profiles/${name}";
+        value = {
+          text = builtins.toJSON {
+            streamKey = profile.streamKey;
+            isPublic = profile.isPublic;
+            motd = profile.motd;
+          };
+          mode = "0644";
+        };
+      }) cfg.profiles.streamProfiles
+    );
+
+    systemd.tmpfiles.rules = lib.mkIf cfg.profiles.enable [
+      "d /etc/broadcast-box 0755 root root -"
+      "d /etc/broadcast-box/profiles 0755 root root -"
+    ];
+
     networking.firewall = {
       allowedUDPPorts = [ cfg.udpPort ];
     };
@@ -67,20 +121,29 @@ in
     virtualisation.oci-containers.containers.broadcast-box = {
       inherit (cfg) image;
       autoStart = true;
+      pull = "newer";
       
       ports = [
         "127.0.0.1:${toString cfg.port}:8080"
         "0.0.0.0:${toString cfg.udpPort}:${toString cfg.udpPort}/udp"
       ];
       
+      volumes = lib.optional cfg.profiles.enable "/etc/broadcast-box/profiles:/broadcast-box/profiles:ro";
+      
       environment = {
         HTTP_ADDRESS = ":8080";
+        HTTP_PORT = "8080";
         ENABLE_HTTP_REDIRECT = "false";
         UDP_MUX_PORT = toString cfg.udpPort;
         NETWORK_TYPES = "udp4";
         NAT_1_TO_1_IP = (builtins.head config.networking.interfaces.ens3.ipv4.addresses).address;
         NETWORK_TEST_ON_START = "false";
-        DISABLE_STATUS = "true";
+        # DISABLE_STATUS = "true";
+
+        # Options for testing merge request with backend reqwite
+        # STREAM_PROFILE_ACTIVE = if cfg.profiles.enable then "true" else "false";
+        # HTTP_ADDRESS = "0.0.0.0";
+        # DEBUG_INCOMING_API_REQUESTS = "true";
       } // cfg.extraEnvironment;
 
       environmentFiles = lib.optional (cfg.environmentFile != null) cfg.environmentFile;
