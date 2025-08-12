a5e98df88b42843af38216532622270dd5124fb3
Sleroq <hireme@sleroq.link>
2025-08-12 21:42:52 +0300

    More refactoring

--- flake.nix
+++ flake.nix
@@ -50,6 +50,7 @@
     disko.inputs.nixpkgs.follows = "nixpkgs-cumserver";
     nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
     mailserver.url = "git+https://gitlab.com/simple-nixos-mailserver/nixos-mailserver.git/";
+    # FIXME: Maybe use overlays to avoid following everything?
     sleroq-link = {
       url = "github:sleroq/sleroq.link";
       inputs.nixpkgs.follows = "nixpkgs-cumserver";
@@ -113,16 +114,33 @@
         {
           hosts = {
             interplanetary = withNixpkgsFor "interplanetary" {
-              tags = [ "desktop" "non-server" ];
+              tags = [ "non-server" ];
+              modules = [
+                inputs.home-manager-interplanetary.nixosModules.home-manager
+                inputs.aagl.nixosModules.default
+                { home-manager.users.sleroq.imports = [ ./home/hosts/interplanetary.nix ]; }
+              ];
             };
 
             international = withNixpkgsFor "international" {
-              tags = [ "laptop" "non-server" ];
+              tags = [ "non-server" ];
+              modules = [
+                inputs.home-manager-international.nixosModules.home-manager
+                { home-manager.users.sleroq.imports = [ ./home/hosts/international.nix ]; }
+              ];
             };
 
             cumserver = withNixpkgsFor "cumserver" {
               arch = "x86_64";
               tags = [ "server" ];
+              modules = [
+                inputs.disko.nixosModules.disko
+                inputs.mailserver.nixosModules.default
+                inputs.reactor.nixosModules.reactor
+                inputs.sieve.nixosModules.sieve
+                inputs.slusha.nixosModules.slusha
+                inputs.nixos-facter-modules.nixosModules.facter
+              ];
               specialArgs = {
                 inherit inputs;
                 secrets = import ./hosts/cumserver/secrets/default.nix;
@@ -132,9 +150,7 @@
 
         shared.modules = [
           (import ./lib/inputs-resolver.nix)
-
           inputs.agenix.nixosModules.default
-
           ({ inputs, ... }: { nixpkgs.overlays = [ inputs.self.overlays.default ]; })
         ];
 
@@ -147,35 +163,10 @@
                     (args // {
                       agenixModule = inputs.agenix.homeManagerModules.default;
                       inputs' = inputsResolved';
-                      self = inputs.self;
+                      inherit (inputs) self;
                       inherit realConfigs;
                     }))
-              ])
-
-              (nixpkgs.lib.optionals (tag == "non-server") [ ./shared ])
-
-              (nixpkgs.lib.optionals (tag == "laptop") [
-                inputs.home-manager-international.nixosModules.home-manager
-                {
-                  home-manager.users.sleroq.imports = [ ./home/hosts/international.nix ];
-                }
-              ])
-
-              (nixpkgs.lib.optionals (tag == "desktop") [
-                inputs.home-manager-interplanetary.nixosModules.home-manager
-                inputs.aagl.nixosModules.default
-                { 
-                  home-manager.users.sleroq.imports = [ ./home/hosts/interplanetary.nix ];
-                }
-              ])
-
-              (nixpkgs.lib.optionals (tag == "server") [
-                inputs.disko.nixosModules.disko
-                inputs.mailserver.nixosModules.default
-                inputs.reactor.nixosModules.reactor
-                inputs.sieve.nixosModules.sieve
-                inputs.slusha.nixosModules.slusha
-                inputs.nixos-facter-modules.nixosModules.facter
+                ./shared
               ])
             ];
 
