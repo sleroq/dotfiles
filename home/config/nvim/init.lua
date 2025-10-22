30a38fd0ffd86bc321f2ee8a2ff1c9ddf2ac1baa
Sleroq <hireme@sleroq.link>
2025-10-22 22:45:12 +0300

    Major config improvements + treesitter

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -8,18 +8,21 @@ vim.o.smartcase = true
 vim.opt.smartindent = true
 vim.opt.undofile = true
 vim.opt.number = true
-vim.opt.relativenumber = true
-vim.o.hlsearch = false -- disables highlight of last search
+-- vim.opt.relativenumber = true
 vim.o.breakindent = true
 vim.o.timeoutlen = 200
 vim.o.completeopt = "menuone,noselect"
 vim.o.termguicolors = true
 vim.o.mouse = ""
+vim.o.updatetime = 2000
 vim.o.swapfile = false
+vim.opt.splitright = true
 vim.opt.expandtab = true -- fuck the tabs
+vim.cmd.packadd("nohlsearch")
 
 -- experimental stuff
-vim.o.path = "**" -- for the find command, maybe helps with completion as well
+vim.o.path = "**"         -- for the find command, maybe helps with completion as well
+vim.opt.lazyredraw = true -- presumably better for ssh
 
 vim.pack.add({
     { src = "https://github.com/nvim-treesitter/nvim-treesitter",        version = "main" },
@@ -40,13 +43,10 @@ vim.pack.add({
     { src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" }, -- TODO: use atleast once?
     { src = "https://github.com/nvim-telescope/telescope-frecency.nvim" },
 
-    { src = "https://github.com/nvim-mini/mini.animate" },
-
     { src = "https://github.com/folke/which-key.nvim" },
     { src = "https://github.com/nvim-lualine/lualine.nvim" },
     -- { src = "https://github.com/folke/todo-comments.nvim" }, -- FIXME: does not work
 
-    { src = "https://github.com/wakatime/vim-wakatime" },
     { src = "https://github.com/j-hui/fidget.nvim" },
 
     -- { src = "https://github.com/neoclide/coc.nvim", version = "release" },
@@ -63,17 +63,19 @@ vim.pack.add({
     -- { src = "hrsh7th/cmp-buffer" },
     -- { src = "hrsh7th/cmp-path" },
     -- { src = "onsails/lspkind.nvim" },
-    -- { src = "https://github.com/supermaven-inc/supermaven-nvim" },
+    { src = "https://github.com/supermaven-inc/supermaven-nvim" },
+    { src = "https://github.com/NickvanDyke/opencode.nvim" },
+    { src = "https://github.com/folke/snacks.nvim" }, -- dep for opencode
 
     { src = "https://github.com/mbbill/undotree" },
     { src = "https://github.com/NMAC427/guess-indent.nvim" },
     -- { src = "https://github.com/folke/trouble.nvim" }, -- TODO: seems usefull
     -- { src = "https://github.com/sigmaSd/deno-nvim" }, -- TODO: enable when I hit some limitation of the default config
+
+    -- https://github.com/coffebar/neovim-project -- maybe this
 })
 
-vim.g.wakatime_CLIPath = "~/.nix-profile/bin/wakatime-cli"
-
-require "guess-indent".setup()
+require "guess-indent".setup({})
 
 -- require "vague".setup({ transparent = true })
 require "vague".setup()
@@ -91,8 +93,9 @@ require "marks".setup {
     mappings = {}
 }
 
--- require("supermaven-nvim").setup({ keymaps = { accept_suggestion = "<C-l>" } })
-require("neoclip").setup({ preview = true; })
+require("supermaven-nvim").setup({ keymaps = { accept_suggestion = "<C-l>" } })
+
+require("neoclip").setup({ preview = true, })
 
 local telescope = require("telescope")
 telescope.setup({
@@ -113,7 +116,7 @@ telescope.setup({
 telescope.load_extension("frecency")
 telescope.load_extension("ui-select")
 
-vim.lsp.config['lua_ls'] = {
+vim.lsp.config["lua_ls"] = {
     settings = {
         Lua = {
             workspace = {
@@ -169,11 +172,11 @@ end
 
 vim.g.mapleader = " "
 map({ "v", "x" }, "<C-y>", '"+y', { desc = "System clipboard yank" })
-map({ "n" }, "<C-x>", '<Cmd>:Telescope neoclip<CR>', { desc = "Clipboard manager" }) -- maybe use registers like a chad instead of this?
+map({ "n" }, "<C-x>", "<Cmd>:Telescope neoclip<CR>", { desc = "Clipboard manager" }) -- maybe use registers like a chad instead of this?
 map({ "n" }, "<leader>/", tsbuiltin.live_grep, { desc = "Live grep" })
-map({ "n" }, "<leader><leader>", "<Cmd>:Telescope frecency workspace=CWD<CR>", { desc = 'Find files' })
-map({ "n" }, "<leader>ff", tsbuiltin.find_files, { desc = 'Find files' })
-map({ "n" }, "<leader>fF", find_noignore, { desc = 'Find files no .gitignore' })
+map({ "n" }, "<leader><leader>", "<Cmd>:Telescope frecency workspace=CWD<CR>", { desc = "Find files" })
+map({ "n" }, "<leader>ff", tsbuiltin.find_files, { desc = "Find files" })
+map({ "n" }, "<leader>fF", find_noignore, { desc = "Find files no .gitignore" })
 map({ "n" }, "<leader>b", tsbuiltin.buffers, { desc = "Find buffers" }) -- is :b tab not enough?
 map({ "n" }, "<leader>n", "<Cmd>:bn<CR>", { desc = "Next buffer" })
 map({ "n" }, "<leader>p", "<Cmd>:bp<CR>", { desc = "Prev buffers" })
@@ -183,32 +186,71 @@ map({ "n" }, "<leader>x", "<Cmd>:bd<CR>", { desc = "Quit the current buffer." })
 map({ "n" }, "<leader>X", "<Cmd>:bd!<CR>", { desc = "Force quit the current buffer." })
 map({ "n" }, "<leader>gg", "<Cmd>:Neogit<CR>", { desc = "Open git shit" })
 map({ "n" }, "gd", vim.lsp.buf.definition, { desc = "Jump to definition" })
-map({ "n" }, "<leader>t", "<Cmd>:terminal<CR>", { desc = "Open terminal" })
+map({ "n" }, "<leader>t", "<Cmd>:vs | te<CR>", { desc = "Open terminal" })
 map({ "n", "v", "x" }, "<leader>gf", vim.lsp.buf.format, { desc = "Format current buffer" })
 map({ "n" }, "<leader>e", "<cmd>Oil<CR>", { desc = "Open oil" })
 
-local animate = require("mini.animate")
-animate.setup({
-    cursor = { timing = animate.gen_timing.linear({ duration = 30, unit = 'total' }) },
-    scroll = { timing = animate.gen_timing.linear({ duration = 30, unit = 'total' }) },
-    resize = { timing = animate.gen_timing.linear({ duration = 30, unit = 'total' }) },
-    open = { timing = animate.gen_timing.linear({ duration = 30, unit = 'total' }) },
-    close = { timing = animate.gen_timing.linear({ duration = 30, unit = 'total' }) },
-})
+if vim.g.neovide then
+    map("n", "<sc-v>", 'l"+P', { noremap = true })
+    map("v", "<sc-v>", '"+P', { noremap = true })
+    map("i", "<sc-v>", '<ESC>"+p', { noremap = true })
+    map("n", "<sc-v>", '"+p', { noremap = true })
+    map("t", "<sc-v>", '<C-\\><C-n>"+Pi', { noremap = true })
+    vim.o.guifont = "JetBrainsMono Nerd Font:h14"
+end
+
+local opencode = require("opencode")
+map({ "n", "x" }, "<leader>oa", function() opencode.ask("@this: ", { submit = true }) end,
+    { desc = "Ask about this" })
+map({ "n", "x" }, "<leader>os", function() opencode.select() end, { desc = "Select prompt" })
+map({ "n", "x" }, "<leader>o+", function() opencode.prompt("@this") end, { desc = "Add this" })
+map("n", "<leader>ot", function() opencode.toggle() end, { desc = "Toggle embedded" })
+map("n", "<leader>oc", function() opencode.command() end, { desc = "Select command" })
+map("n", "<leader>on", function() opencode.command("session_new") end, { desc = "New session" })
+map("n", "<leader>oi", function() opencode.command("session_interrupt") end, { desc = "Interrupt session" })
+map("n", "<leader>oA", function() opencode.command("agent_cycle") end, { desc = "Cycle selected agent" })
+map("n", "<S-C-u>", function() opencode.command("messages_half_page_up") end,
+    { desc = "Messages half page up" })
+map("n", "<S-C-d>", function() opencode.command("messages_half_page_down") end,
+    { desc = "Messages half page down" })
 
 -- disable ts_ls for deno project
 vim.api.nvim_create_autocmd("User", {
-  pattern = "LspAttach",
-  callback = function()
-    local cwd = vim.fn.getcwd()
-    if cwd:match("slusha") then
-      vim.cmd("LspStop ts_ls")
-      vim.cmd("LspStart denols")
-    end
-  end,
+    pattern = "LspAttach",
+    callback = function()
+        local cwd = vim.fn.getcwd()
+        if cwd:match("slusha") then
+            vim.cmd("LspStop ts_ls")
+            vim.cmd("LspStart denols")
+        end
+    end,
 })
 
--- map({ "n" }, "<leader>c", "1z=")
--- map({ "n" }, "<C-q>", ":copen<CR>", { silent = true })
--- map({ "n" }, "<leader>w", "<Cmd>update<CR>", { desc = "Write the current buffer." })
--- map({ "n" }, "<C-f>", "<Cmd>Open .<CR>", { desc = "Open current directory in Finder." })
+local ts_parsers = {
+    "go", "gomod", "gosum", "vim", "vimdoc", "javascript",
+    "zig", "typescript", "json", "dockerfile", "sql",
+    "yaml", "bash", "gitignore", "prisma",
+}
+local nts = require("nvim-treesitter")
+nts.install(ts_parsers)
+
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function(args)
+        local filetype = args.match
+        local lang = vim.treesitter.language.get_lang(filetype)
+        if lang and vim.treesitter.language.add(lang) then
+            vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"               -- folds, provided by Neovim
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation, provided by nvim-treesitter
+            vim.treesitter.start()                                            -- syntax highlighting, provided by Neovim
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("PackChanged", { callback = function() nts.update() end })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*sh.example",
+    callback = function()
+        vim.bo.filetype = "sh"
+    end,
+})
