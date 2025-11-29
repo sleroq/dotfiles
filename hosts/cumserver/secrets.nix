0601a29ac9beb87933e4f9ad1619b13c29fee1d0
Sleroq <hireme@sleroq.link>
2025-11-29 12:08:20 +0300

    Added another key

--- hosts/cumserver/secrets.nix
+++ hosts/cumserver/secrets.nix
@@ -1,7 +1,8 @@
 let
   sleroq = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK72BBTnP5Os5ZQfS1BuigNzWMqNFl7lgUH4CJq1bl9P cantundo@pm.me";
+  portable = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDSh54pu9bAH8DFBKPtswFJzevCft+gHZStJQ0trYGoj sleroq@cum.army";
   cumserver = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGNy4lHOvczy/vR4hf+uk6ciJGpkw5mqu3oC+9hTDbqf";
-  commonPublicKeys = [ sleroq cumserver ];
+  commonPublicKeys = [ sleroq portable cumserver ];
 
   numberOfMailPasswords = 5;
   mailSecretBasePath = "secrets/mail/";
