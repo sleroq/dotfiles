535258b30ade05a693c8a1c7f3deb7d920e34e77
Sleroq <hireme@sleroq.link>
2026-01-07 02:18:02 +0300

    Added "fix" for sound in mpv

--- home/modules/programs/mpv.nix
+++ home/modules/programs/mpv.nix
@@ -4,6 +4,9 @@
   programs.mpv = {
     enable = true;
     package = pkgs.mpv;
+    bindings = {
+      "n" = "cycle_values af \"lavfi=[loudnorm=I=-16:TP=-1.5:LRA=11]\"";
+    };
     config = {
       volume = 50;
       osd-font-size = 24;
