87673487e58e7a89e6bdb76a7fd12bd4a3010b97
Sleroq <hireme@sleroq.link>
2026-04-16 13:02:14 +0300

    Install using bun instead of using nixpkgs

--- home/modules/programs/pi.nix
+++ home/modules/programs/pi.nix
@@ -1,8 +1,8 @@
 {
-  pkgs,
   lib,
   config,
   opts,
+  pkgs,
   ...
 }:
 
@@ -14,14 +14,25 @@ in
     enable = lib.mkEnableOption "Pi coding agent";
   };
 
-  config = lib.mkIf cfg.enable {
-    home.packages = [ pkgs."pi-coding-agent" ];
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      {
+        home.activation.installPi = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          PATH="${pkgs.bun}/bin:$HOME/.bun/bin:$PATH"
+          if ! command -v pi &> /dev/null; then
+            run ${pkgs.bun}/bin/bun install -g @mariozechner/pi-coding-agent
+          fi
+        '';
+      }
 
-    home.activation.piConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      mkdir -p $HOME/.pi/agent
+      {
+        home.activation.piConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          mkdir -p $HOME/.pi/agent
 
-      $DRY_RUN_CMD ln -sfn $VERBOSE_ARG \
-          ${opts.realConfigs}/pi/agent/* $HOME/.pi/agent/
-    '';
-  };
+          $DRY_RUN_CMD ln -sfn $VERBOSE_ARG \
+              ${opts.realConfigs}/pi/agent/* $HOME/.pi/agent/
+        '';
+      }
+    ]
+  );
 }
